d8b297e4616ddc209ea0229330a912b44fa798f9
Bharat Firake <bharatfirake99@gmail.com>
2023-07-16 12:37:56 +0530

    Days Of Week

---
+++ DaysOfWeek/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+namespace DaysOfWeek
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string[] daysOfWeek = {
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+        };
+
+            var dayNames = from day 
+                           in daysOfWeek
+                           select day;
+            Console.WriteLine("Days of Week are : ");    
+            foreach (var name in dayNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
